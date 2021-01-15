5c48b8c822b5e218f40a5a938bce9b47f86e157c
Habib <mohamedmosaadhabib@gmail.com>
2021-01-15 02:28:37 +0200

    init

---
+++ tests/AbstractFactoryTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AbstractFactoryTest extends TestCase
+{
+
+    public function testIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+}
